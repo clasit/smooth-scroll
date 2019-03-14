a24c2893ed0bd3e1a00d6e8c820a809322101fd9
Jesús <jesus.m.f@outlook.com>
2019-03-14 22:11:23 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'smooth-scroll';
+  elementList = [
+    1, 2, 3, 4, 5, 
+    6 ,7 , 8 , 9, 10,
+    11, 12, 13, 14, 15, 
+    16, 17, 18, 19, 20
+  ];
+
+  public selectItem(e) {
+    // (1) Get item
+    const item: HTMLElement = e.target;
+    const container: HTMLElement = item.parentElement;
+    const frame: HTMLElement = container.offsetParent as HTMLElement;
+
+    // item.scrollIntoView({ behavior: 'smooth' });
+    console.log(container.scrollLeft, item.scrollLeft);
+    console.log(`item Offset -> ${item.offsetLeft}`);
+
+    console.log(container.offsetWidth, frame.offsetWidth);
+    container.scrollLeft = item.offsetLeft + (frame.offsetWidth / 2);
+
+    // debugger
+  }
 }
